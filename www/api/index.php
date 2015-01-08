26ed1eafb8151188c0b777b8585b6a60e31bf6c6
Gonzalo Ayuso <gonzalo123@gmail.com>
2015-01-08 20:40:02 +0100

    clean

--- www/api/index.php
+++ www/api/index.php
@@ -3,10 +3,8 @@
 include __DIR__ . '/../../vendor/autoload.php';
 
 use Silex\Application;
-use G\AngularPostRequestServiceProvider;
 
 $app = new Application();
-$app->register(new AngularPostRequestServiceProvider());
 
 $app->get("/", function (Application $app) {
     return $app->json([
